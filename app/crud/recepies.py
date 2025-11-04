2ba8a37863e34eb085e1146e3f48c2c3b8b075bd
Максим Лучко <maksimus247@gmail.com>
2025-11-04 16:28:35 +0200

    made crud file for recepies

---
+++ app/crud/recepies.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+from sqlalchemy import and_, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from uuid import UUID
+from app.crud.base import CRUDBase
+from app.models.recipes import Recipe
+from app.schemas.recepies import RecipeCreate, RecipeUpdate
+
+
+class CRUDRecipe(CRUDBase[Recipe, RecipeCreate, RecipeUpdate]):
+    async def get_multi_by_owner(self, db:AsyncSession, *, user_id:UUID, skip:int=0, limit:int=100) -> List[Recipe]:
+        res = await db.execute(select(Recipe).where(Recipe.user_id == user_id).offset(skip).limit(limit))
+        return res.scalars().all()
+    
+    
+    async def get_public_recipes(self, db:AsyncSession, *, skip:int=0, limit:int=100) -> List[Recipe]:
+        res = await db.execute(select(Recipe).where(Recipe.is_public==True).offset(skip).limit(limit))
+        return res.scalars().all()
+    
+    
+    async def search_by_title(self, db:AsyncSession, *, title:str, user_id:Optional[UUID] = None, skip:int=0, limit:int=100):
+        filters = [Recipe.title.ilike(f"%{title}")]
+        if user_id:
+            filters.append(Recipe.user_id == user_id)
+        
+        res = await db.execute(select(Recipe).where(and_(*filters)).offset(skip).limit(limit))
+        return res.scalars().all()
+    
+
+recipe = CRUDRecipe(Recipe)
